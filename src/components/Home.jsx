cb249be705ae9a5c3105d41031640e87e736b46e
Nicolas787 <elnicolas787@gmail.com>
2024-03-19 16:07:57 +0300

    By Nicolas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
           </p>
 
           <div className='mt-10'>
-            <button className='btn font-semibold outline-none bg-blue-400 py-2 px-4 text-white rounded-lg hover:bg-white hover:text-blue-400 transition-all duration-500'>Download CV</button>
+            <a href="/assets/files/CV_Nicolas.pdf" download="CV Nicolas" className='btn font-semibold outline-none bg-blue-400 py-2.5 px-4 text-white rounded-lg hover:bg-white hover:text-blue-400 transition-all duration-500'>Download CV</a>
             <Link
               activeClass="active"
               to="experience"
