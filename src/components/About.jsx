f2ad932732940c5f3901cef5b178c70cbc3535f5
Nicolas.Ibrahim <73852999+elnicolas-787@users.noreply.github.com>
2025-02-18 08:08:12 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,19 @@ function About() {
   const skills = [
     {
       id: 1,
-      name: "UI & UX DESIGNING",
+      name: "UI & UX",
       image: "/assets/icons/design.png",
       alt: "Design",
     },
     {
       id: 2,
-      name: "WEB DEVELOPMENT",
+      name: "WEB",
       image: "/assets/icons/web-dev.png",
       alt: "Web Development",
     },
     {
       id: 3,
-      name: "MOBILE DEVELOPMENT",
+      name: "MOBILE",
       image: "/assets/icons/mob-dev.png",
       alt: "Mobile Development",
     },
@@ -27,21 +27,16 @@ function About() {
     <div className="px-7 md:px-10 sm:mt-20 md:-mt-52" id="about">
       <div className="max-w-7xl mx-auto pt-20">
         <h1 className="text-3xl md:text-5xl my-5 text-blue-400 font-semibold">
-          About me
+          À propos de moi
         </h1>
         <p className="text-white my-2 md:w-2/3 leading-[2]">
-          Hi, my name is RANDRIAFANANTENANTSOA Tahirinirina Nicolas, i am a
-          Fullstack web developer, UI designer, and Mobile developer. I have
-          honed my skills in web Development and advance i have core
-          understanding of advance UI design principales. Here are the major
-          skills i have.{" "}
+          Bonjour, je m'appelle RANDRIAFANANTENANTSOA Tahirinirina Nicolas, je suis développeur web Fullstack, designer UI et développeur mobile. J'ai perfectionné mes compétences en développement web et j'ai une compréhension approfondie des principes avancés du design UI. Voici les principales compétences que je possède.{" "}
         </p>
 
         <div className="md:flex items-center my-7">
-          <p className="text-blue-400 text-8xl font-bold">1+</p>
+          <p className="text-blue-400 text-8xl font-bold">2+</p>
           <p className="text-white text-lg sm:text-2xl md:ml-5">
-            Years of experience. Specialised in building apps, while ensuring a
-            seamless web experience for end users.
+            Des années d'expérience. Spécialisé dans la création d'applications, tout en garantissant une expérience web fluide pour les utilisateurs finaux.
           </p>
         </div>
 
