6cdeead7e61785536be89ca47fd4ae58341b5b3e
Nicolas.Ibrahim <73852999+elnicolas-787@users.noreply.github.com>
2025-02-18 08:39:20 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
               Développeur fullstack. Je crée une expérience web fluide pour les utilisateurs finaux.
             </p>
 
-            <div className="max-sm:block mt-10">
+            <div className="flex flex-col sm:flex-row sm:justify-start sm:space-x-5 mt-10">
               <a
                 href="/assets/files/CV_Nicolas.pdf"
                 download="CV Nicolas"
