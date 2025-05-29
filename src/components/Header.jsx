e88de66c2a2c111bca54823ae7a94d59fc58a899
Nicolas.Ibrahim <73852999+elnicolas-787@users.noreply.github.com>
2025-05-29 12:43:06 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,43 +14,47 @@ function Header() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="w-full fixed z-50 bg-black">
+    <header className="w-full fixed z-50 bg-black md:px-10 px-7">
       <div className="max-w-7xl mx-auto">
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
-        {/* Section logo */}
-        <div className="font-bold md:text-4xl text-2xl cursor-pointer flex items-center gap-1">
-          <span className="text-blue-400 hover:text-white transition-all duration-500">
-            Nicolas
-          </span>
-        </div>
+        <div className="md:flex items-center justify-between py-4">
+          {/* Section logo */}
+          <div className="font-bold md:text-4xl text-2xl cursor-pointer flex items-center gap-1">
+            <span className="text-blue-400 hover:text-white transition-all duration-500">
+              Nicolas
+            </span>
+          </div>
 
-        {/* Mobile menu icon */}
-        <div
-          onClick={() => setOpen(!open)}
-          className="w-7 h-7 right-8 top-5 cursor-pointer md:hidden text-white absolute"
-        >
-          {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
-        </div>
+          {/* Mobile menu icon */}
+          <div
+            onClick={() => setOpen(!open)}
+            className="w-7 h-7 right-8 top-5 cursor-pointer md:hidden text-white absolute"
+          >
+            {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
+          </div>
 
-        {/* Link items */}
-        <ul className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-9 bg-gray-600 md:bg-transparent transition-all duration-500 ease-in ${open ? "top-12" : "top-[-490px]"}`}>
-          {links.map((link) => (
-            <li key={link.id} className="md:ml-8 md:my-0 my-7 font-semibold">
-              <Link
-                activeClass="active"
-                to={link.link}
-                spy={true}
-                smooth={true}
-                offset={50}
-                duration={500}
-                className="py-2 text-white text-lg cursor-pointer hover:text-blue-400 link-underline transition-all duration-500"
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+          {/* Link items */}
+          <ul
+            className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-9 bg-gray-600 md:bg-transparent transition-all duration-500 ease-in ${
+              open ? "top-12" : "top-[-490px]"
+            }`}
+          >
+            {links.map((link) => (
+              <li key={link.id} className="md:ml-8 md:my-0 my-7 font-semibold">
+                <Link
+                  activeClass="active"
+                  to={link.link}
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="py-2 text-white text-lg cursor-pointer hover:text-blue-400 link-underline transition-all duration-500"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
